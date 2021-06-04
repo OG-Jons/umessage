cb43f7100d48bd864f3ce3c23ed6d8e8db3bbb83
OG-Jons <jonas@marschall.pro>
2021-06-04 09:54:29 +0200

    Added Material-UI and a Button for display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained" color="primary">Simple Button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
