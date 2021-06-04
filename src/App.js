f7f10cf7880489b5e28f1e2eabc0e8b1a582917d
OG-Jons <jonas@marschall.pro>
2021-06-04 10:31:29 +0200

    Added firebase and firebaseConfig.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {firebase} from "./firebaseConfig";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const firebaseApp = firebase.apps[0];
+    console.log(firebaseApp)
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <code>
+                    very nais
+                </code>
+            </header>
+        </div>
+    );
 }
 
 export default App;
