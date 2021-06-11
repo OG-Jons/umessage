d4fb4bcc597543df03266716d869e8aa0b880b8e
Nick Zimmermann <nick.zimmermann@swisscom.com>
2021-06-11 16:36:36 +0200

    fixed TextfieldChatbox, change MainHeader

--- src/components/MainHeader/MainHeader.component.jsx
+++ src/components/MainHeader/MainHeader.component.jsx
@@ -1,73 +1,116 @@
-import { AppBar, makeStyles, Toolbar, Typography, IconButton, Hidden, Drawer } from '@material-ui/core';
-import { useState } from 'react';
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Brightness2OutlinedIcon from '@material-ui/icons/Brightness2Outlined';
-import WbSunnyOutlinedIcon from '@material-ui/icons/WbSunnyOutlined';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import Sidebar from '../Sidebar/Sidebar.component';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+
+const drawerWidth = 250;
 
 const useStyles = makeStyles((theme) => ({
-  header: {
-    position: 'absolute',
-    backgroundColor: '#F6F6F6',
-    borderBottomLeftRadius: '15px',
-    borderBottomRightRadius: '15px'
+  root: {
+    display: 'flex'
   },
-  logo: {
-    fontFamily: 'Work Sans, sans-serif',
-    fontWeight: 600,
-    color: '#000000',
-    margin: 'auto',
-    textAlign: 'center'
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+      top: '15%'
+    }
   },
-
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: '100%',
+      marginLeft: drawerWidth,
+      zIndex: theme.zIndex.drawer + 1,
+    }
+  },
   menuButton: {
     marginRight: theme.spacing(2),
     [theme.breakpoints.up('sm')]: {
       display: 'none'
     }
   },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  }
 
-  moon: {
-    marginRight: theme.spacing(2),
-    color: '#000000'
-  },
-
-  profile: {
-    marginRight: theme.spacing(2),
-    color: '#000000'
+  logo: {
+    textAlign: "center"
   }
 }));
 
-export default function MainHeader({ onClickHamburger, hamburger, onClickDarkmode, darkmode }) {
-  const { header, logo, menuButton, moon, profile } = useStyles();
-  const [mobileOpen, setMobileOpen] = useState(false);
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  return (
+  const drawer = (
     <div>
-      <AppBar className={header}>
-        <Toolbar>
-          <Typography variant="h4" className={logo}>
+      <div className={classes.toolbar} />
+        Toolbar
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar className={classes.toolBar}>
+          <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
             uMessage
           </Typography>
-          {darkmode ? (
-            <IconButton edge="end" className={moon} color="inherit" arial-label="moon" onClick={onClickDarkmode}>
-              <WbSunnyOutlinedIcon />
-            </IconButton>
-          ) : (
-            <IconButton edge="end" className={moon} color="inherit" arial-label="moon" onClick={onClickDarkmode}>
-              <Brightness2OutlinedIcon />
-            </IconButton>
-          )}
-          <IconButton edge="end" className={profile} color="inherit" aria-label="profile">
-            <AccountCircleIcon />
-          </IconButton>
         </Toolbar>
       </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
     </div>
   );
 }
+
+export default ResponsiveDrawer;
--- src/components/TextfieldChatbox/TextFieldChatbox.component.jsx
+++ src/components/TextfieldChatbox/TextFieldChatbox.component.jsx
@@ -1,25 +1,53 @@
-import { TextField } from '@material-ui/core';
+import { makeStyles, TextField, Box, IconButton } from '@material-ui/core';
 import React, { useState } from 'react';
-import './TextfieldChatbox.style.css';
 import SendIcon from '@material-ui/icons/Send';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.up('sm')]: {
+      width: 'calc(100% - 290px)',
+      marginLeft: 270
+    },
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    backgroundColor: 'rgb(177, 173, 173)',
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: '100%'
+  },
+  input: {
+    width: '90%',
+    marginLeft: 10
+  },
+  container: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
 export default function TextFieldChatbox() {
+  const classes = useStyles();
   const [input, setInput] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(input.trim() === '') return;
+    if (input.trim() === '') return;
     console.log(input);
     setInput('');
   };
   return (
-    <div className="text-field">
+    <Box className={classes.root}>
       <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <div className="container">
-          <TextField className="input" label="Message:" value={input} onChange={(e) => setInput(e.target.value)} />
-          <SendIcon onClick={handleSubmit} type="submit" className="icon-send" />
-        </div>
+        <Box className={classes.container}>
+          <TextField className={classes.input} label="Message:" value={input} onChange={(e) => setInput(e.target.value)} />
+          <IconButton onClick={handleSubmit}>
+            <SendIcon />
+          </IconButton>
+        </Box>
       </form>
-    </div>
+    </Box>
   );
 }
