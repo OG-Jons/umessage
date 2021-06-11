0e65c5fc22e21716f261f1cabca658a364862f79
Nick Zimmermann <nick.zimmermann@swisscom.com>
2021-06-11 14:37:03 +0200

    sidebar not working anymore, removed hamburger, message text added

---
+++ src/components/HeaderChatbox/headerChatBox.component.jsx
@@ -0,0 +1,13 @@
+import { Toolbar, AppBar } from '@material-ui/core';
+
+function headerChatBox() {
+    return (
+        <AppBar>
+            <Toolbar>
+              
+            </Toolbar>            
+        </AppBar>
+    );
+ }
+
+export default headerChatBox;
