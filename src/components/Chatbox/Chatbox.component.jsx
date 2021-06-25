56d09c976b6d2ec4a3bfc0d6757d321eb8276a3c
Alejandro Lüthi <alejandro.luethi@namics.com>
2021-06-25 08:13:12 +0200

    Message displays correctly

--- src/components/Chatbox/Chatbox.component.jsx
+++ src/components/Chatbox/Chatbox.component.jsx
@@ -1,11 +1,13 @@
 import TextFieldChatbox from '../TextfieldChatbox/TextFieldChatbox.component';
 import HeaderChatBox from "../HeaderChatbox/HeaderChatBox.component";
 import './Chatbox.style.css';
+import Message from '../Message/Message.component'
 
 export default function Chatbox() {
     return (
         <div>
             <HeaderChatBox />
+            <Message />
             <TextFieldChatbox />
         </div>
     )
--- src/components/Message/Message.component.jsx
+++ src/components/Message/Message.component.jsx
@@ -1,8 +1,36 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        //minWidth: 'min-content',
+        width: 'max-content',
+        maxWidth: '40vw',
+    },
+
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+});
+
 export default function Message() {
+    const classes = useStyles();
 
     return (
-        <>
-            Message
-        </>
-    )
+        <Card className={classes.root} variant="outlined">
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                    Name
+                </Typography>
+                <Typography variant="body2" component="p">
+                    Messages ssssssssssssssssssssss kkkkkkk
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 }
