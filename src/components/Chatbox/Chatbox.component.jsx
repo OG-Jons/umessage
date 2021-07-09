9d4d663148708f8d3aa3d0c9e268781ae9d5ab98
OG-Jons <jonas@marschall.pro>
2021-07-09 15:51:41 +0200

    message getter changes for chat

--- src/components/Chatbox/Chatbox.component.jsx
+++ src/components/Chatbox/Chatbox.component.jsx
@@ -6,63 +6,16 @@ import {db} from '../../server/firebaseConfig';
 import {useCollectionData} from 'react-firebase-hooks/firestore';
 
 export default function Chatbox(props) {
-	const data = [{
-		id: 1,
-		message: 'hey',
-		name: 'Fabian'
-	},
-	{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: 'Fabian isch en spasst',
-		name: 'Nick'
-	},{
-		id: 2,
-		message: ' Fabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasstFabian isch en spasst',
-		name: 'Nick'
-	},
-	];
+	const {chat} = props;
 
-	const messagesRef = db.collection('messages').document(props.chat).collection('messages');
+	const messagesRef = db.collection(`chat/${chat}/messages`);
 	const query = messagesRef.orderBy('createdAt');
 	const [messages] = useCollectionData(query, { idField: 'id' });
-	console.log(messages);
 
 	return (
 		<div>
 			<HeaderChatBox/>
-			<Message messages={data} />
+			<Message messages={messages} />
 			<TextFieldChatbox/>
 		</div>
 	);
